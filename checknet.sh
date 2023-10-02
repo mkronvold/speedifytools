3bf33953c5e498feb18bf73975dd4cebaecc734e
mkronvold <>
2023-10-02 14:31:06 -0500

    added -q quiet flag

--- checknet.sh
+++ checknet.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[ "$1" == "-q" ] && QUIET=1 || QUIET=0
+
 datestamp=$(date +'%Y-%m-%d_%H:%M:%S')
 wget -q --tries=3 --timeout=3 --spider http://bing.com
 if [[ $? -eq 0 ]]; then
-  echo "[${datestamp}] Online"
+  [ $QUIET ] || echo "[${datestamp}] Online"
   exit 0
 else
   echo "[${datestamp}] Offline"
