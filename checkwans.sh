32b4386d63d53bed52f42365d50a5f6bd14605a1
mkronvold <>
2023-11-10 15:04:05 -0600

    added logging

--- checkwans.sh
+++ checkwans.sh
@@ -12,6 +12,8 @@ TARGET_NACR2=173.230.126.7
 in=/root/.wan_interfaces.csv
 wemo=/root/.bin/wemo.sh
 
+[ "$1" == "--nolog" ] && unset LOG || LOG=/var/log/reconnect.log
+[ "$1" == "--log" ] && [ ! "$2" == "" ] && LOG=$2
 
 # Establish some timestamps
 today=$(date +'%Y-%m-%d')
@@ -29,23 +31,23 @@ do
   unset current_ipaddr
   current_ipaddr=$(ip route list match default dev $interface | awk '{print $7}')
   if [ "${current_ipaddr}" == "" ]; then
-    echo $today $now $interface down
+    [ $LOG ] && echo $today $now $interface down >> $LOG || echo $today $now $interface down 
     #$wemo 10.0.0.118 getstate
-    $wemo 10.0.0.118 off
+    [ $LOG ] && ( echo -n $today $now $interface" " ; $wemo 10.0.0.118 off ) >> $LOG || ( echo -n $today $now $interface" " ; $wemo 10.0.0.118 off )
     sleep 3
-    $wemo 10.0.0.118 on
+    [ $LOG ] && ( echo -n $today $now $interface" " ; $wemo 10.0.0.118 on ) >> $LOG || ( echo -n $today $now $interface" " ; $wemo 10.0.0.118 on )
   else
-    echo $today $now $interface up, checking gateway
+    [ $LOG ] && echo $today $now $interface up, checking gateway >> $LOG || echo $today $now $interface up, checking gateway
     unset current_gateway
     current_gateway=$(ip route list match default dev $interface | awk '{print $3}')
     # match the gateway to see if the csv needs an update
     if [ "$current_gateway" == "$stored_gateway" ]; then
-      echo $today $now $interface gateway valid
-      echo $today $now $interface up
+      [ $LOG ] && echo $today $now $interface gateway valid >> $LOG || echo $today $now $interface gateway valid
+      [ $LOG ] && echo $today $now $interface up >> $LOG || echo $today $now $interface up
     else
-      echo $today $now $interface not valid, saving new gateway and rechecking
+      [ $LOG ] && echo $today $now $interface not valid, saving new gateway and rechecking >> $LOG || echo $today $now $interface not valid, saving new gateway and rechecking
       netstat -rn | grep -v connectify | grep UG | grep "0.0.0.0" |  awk '{print $8 "," $2}' | sort -u > $in.tmp
     fi
   fi
 done < $in
-[ -f ${in}.tmp ] && (echo $today $now ; mv -v ${in}.tmp $in)
+[ -f ${in}.tmp ] && mv ${in}.tmp $in
