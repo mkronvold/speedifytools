5effdd880bc1d0ce5f506a59146b01d44c29e37d
mkronvold <>
2023-11-29 18:08:32 -0600

    moved log to variable, updated runcmd for new speedify options

--- speedify_watchdog.sh
+++ speedify_watchdog.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-BYPASS=1
+BYPASS=
 
 [ "$BYPASS" ] && exit 
 
+LOG=/tmp/speedify_watchdog.log
 DEBUG=
 
-[ "$DEBUG" ] || RUNCMD="/usr/lib/speedifyconf/run.sh"
+[ "$DEBUG" ] || RUNCMD="/usr/lib/speedifyconf/run.sh restart"
 [ "$DEBUG" ] || WATCHCMD="/usr/share/speedify/speedify_cli"
 [ "$DEBUG" ] || WATCHARGS="state"
 [ "$DEBUG" ] || TIMEOUT=30
@@ -16,4 +17,6 @@ DEBUG=
 [ "$DEBUG" ] && TIMEOUT=5
 OPTIONS="-v --signal=9"
 
-timeout ${OPTIONS} ${TIMEOUT}s "${WATCHCMD}" "${WATCHARGS}" 2>> /tmp/speedify_watchdog.log 1>> /tmp/speedify_watchdog.log && echo "" || ${RUNCMD} 1>> /tmp/speedify_watchdog.log 2>> /tmp/speedify_watchdog.log
+echo -n "$(date +'%Y-%m-%d_%H:%M:%S') " >> $LOG
+
+timeout ${OPTIONS} ${TIMEOUT}s "${WATCHCMD}" "${WATCHARGS}" 2>> $LOG 1>> $LOG && echo "" || ${RUNCMD} 1>> $LOG 2>> $LOG
